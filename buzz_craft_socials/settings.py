f8d2ccb42ecfbd0d590c52e7900d88e895d4b8e2
bhargav13a <a13bhargav@gmail.com>
2025-11-25 02:57:28 +0530

    third upload

--- buzz_craft_socials/settings.py
+++ buzz_craft_socials/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9g!^7%3+io1@kd4feocc-=pesld!%gj_2^dyr_=-c-&z9ww76e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,4 +128,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
