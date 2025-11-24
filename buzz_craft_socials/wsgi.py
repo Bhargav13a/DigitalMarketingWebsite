c2b594ddce64fed9e2cdaa43e227173e5586a16c
bhargav13a <a13bhargav@gmail.com>
2025-11-25 03:54:19 +0530

    Add render.yaml

--- buzz_craft_socials/wsgi.py
+++ buzz_craft_socials/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buzz_craft_socials.settings')
 
 application = get_wsgi_application()
+
+app = application
