34933712b80f53bc5221202b211618b321404598
bhargav13a <a13bhargav@gmail.com>
2025-11-25 04:55:54 +0530

    final

--- buzz_craft_socials/wsgi.py
+++ buzz_craft_socials/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buzz_craft_socials.settings')
 
+application = get_wsgi_application()
+
+
+# add your project to the path
+path = '/home/Bhargav13a/DigitalMarketingWebsite'
+if path not in sys.path:
+    sys.path.append(path)
+
+# set DJANGO_SETTINGS_MODULE
+os.environ['DJANGO_SETTINGS_MODULE'] = 'your_project_name.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
