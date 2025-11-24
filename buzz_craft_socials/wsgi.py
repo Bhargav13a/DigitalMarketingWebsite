89f14ec8bf718a521c66e7446383db981d97295b
bhargav13a <a13bhargav@gmail.com>
2025-11-25 03:44:23 +0530

    fourth changes

--- buzz_craft_socials/wsgi.py
+++ buzz_craft_socials/wsgi.py
@@ -14,5 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buzz_craft_socials.settings')
 
 application = get_wsgi_application()
-
-app = application
